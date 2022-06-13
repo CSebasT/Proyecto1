32f8115abdd668ff0a878ea560f1377d8b2a4ce6
CSebasT <107124936+CSebasT@users.noreply.github.com>
2022-06-13 16:56:26 -0500

    Update

---
+++ src/interfazGra/Interfaz.java
@@ -0,0 +1,13 @@
+package interfazGra;
+
+/**
+ *
+ * @author USER
+ */
+public class Interfaz {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo");
+    }
+}
