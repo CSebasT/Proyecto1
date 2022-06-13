da5859c9bf427db5589cd87249cb1b9cf23ff2b2
CSebasT <107124936+CSebasT@users.noreply.github.com>
2022-06-13 16:55:02 -0500

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author USER
- */
 public class Main {
 
     /**
@@ -14,7 +5,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hola");
+        System.out.println("Hola mundo");
     }
     
 }
